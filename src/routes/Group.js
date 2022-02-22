06e2c354f6013e391b4028b5617c5d059949f138
youngdong82 <nfs82young@gmail.com>
2022-02-22 11:51:13 +0900

    220222 Refactoring - create ShiftInfo.js

--- src/routes/Group.js
+++ src/routes/Group.js
@@ -6,6 +6,8 @@ import hang from '../asset/group/poster_hang.jpg';
 import death from '../asset/group/poster_death.jpg';
 import '../style/web/group.css'
 import '../style/mobile/group_m.css'
+//function
+import ShiftInfo from '../function/ShiftInfo';
 //data
 import Exhi_INFO from '../data/Exhi_INFO';
 
@@ -13,22 +15,10 @@ import Exhi_INFO from '../data/Exhi_INFO';
 
 const Group = () =>  {
   const EXHI = Exhi_INFO(1);
-  const [info, setInfo] = useState('');
-
-  const shiftInfo = async(e) => {
-    const {target: {alt}} = e;
-    if (alt === EXHI[0].title){
-      setInfo(EXHI[0])
-    } else if(alt === EXHI[1].title){
-      setInfo(EXHI[1])
-    } else if(alt === EXHI[2].title){
-      setInfo(EXHI[2])
-    } else if(alt === EXHI[3].title){
-      setInfo(EXHI[3])
-    } else if(alt === EXHI[4].title){
-      setInfo(EXHI[4])
-    }
-  };
+  
+  // shiftInfoPack[0]은 현재 info
+  // shiftInfoPack[1]은 info 바꾸는 함수
+  const shiftInfoPack = ShiftInfo(EXHI);
 
 
   return (
@@ -38,22 +28,22 @@ const Group = () =>  {
       </div>
       <div className="group_reck">
           <div className="each_group">
-            <img className="group_img" src={death} alt="나의 사인이 너와 같다면" onMouseEnter={shiftInfo} />
+            <img className="group_img" src={death} alt="나의 사인이 너와 같다면" onMouseEnter={shiftInfoPack[1]} />
           </div>
 
           <div className="each_group">
-            <img className="group_img" src={hang} alt="P는 그림을 걸었다" onMouseEnter={shiftInfo} />
+            <img className="group_img" src={hang} alt="P는 그림을 걸었다" onMouseEnter={shiftInfoPack[1]} />
           </div>
 
           <div className="each_group">
-            <img className="group_img" src={sing} alt="노래하던 새들도 지금은 사라지고" onMouseEnter={shiftInfo} />
+            <img className="group_img" src={sing} alt="노래하던 새들도 지금은 사라지고" onMouseEnter={shiftInfoPack[1]} />
           </div>
 
           <div className="each_group">
-            <img className="group_img_s" src={hungry} alt="어쨌든 아주 배가 고팠던 모양이구나" onMouseEnter={shiftInfo} />
+            <img className="group_img_s" src={hungry} alt="어쨌든 아주 배가 고팠던 모양이구나" onMouseEnter={shiftInfoPack[1]} />
           </div>
       </div>
-      <ExhiInfo info={info} />
+      <ExhiInfo info={shiftInfoPack[0]} />
     </section>
   );
 }
--- src/routes/Works.js
+++ src/routes/Works.js
@@ -14,27 +14,16 @@ import '../style/web/works.css';
 import '../style/mobile/works_m.css';
 // function
 import { useHorizontalScroll } from "../function/scroll";
+import ShiftInfo from '../function/ShiftInfo';
 //data
 import Exhi_INFO from '../data/Exhi_INFO';
 
 const Works = () =>  {
   const EXHI = Exhi_INFO(0);
-  const [info, setInfo] = useState('');
 
-  const shiftInfo = async(e) => {
-    const {target: {alt}} = e;
-    if (alt === EXHI[0].title){
-      setInfo(EXHI[0])
-    } else if(alt === EXHI[1].title){
-      setInfo(EXHI[1])
-    } else if(alt === EXHI[2].title){
-      setInfo(EXHI[2])
-    } else if(alt === EXHI[3].title){
-      setInfo(EXHI[3])
-    } else if(alt === EXHI[4].title){
-      setInfo(EXHI[4])
-    }
-  };
+  // shiftInfoPack[0]은 현재 info
+  // shiftInfoPack[1]은 info 바꾸는 함수
+  const shiftInfoPack = ShiftInfo(EXHI);
 
   const scrollRef = useHorizontalScroll();
 
@@ -47,35 +36,35 @@ const Works = () =>  {
       <div className="works_exhi" ref={scrollRef}>
         <Link to='/muhmdapinammo'>
           <div className="img_index">
-            <img className="works_exhi_index1" src={omma_main} alt="MUH EMDAP INAM MO" onMouseEnter={shiftInfo} />
-            <img className="works_exhi_index2" src={omma_sec} alt="MUH EMDAP INAM MO" onMouseEnter={shiftInfo} />
+            <img className="works_exhi_index1" src={omma_main} alt="MUH EMDAP INAM MO" />
+            <img className="works_exhi_index2" src={omma_sec} alt="MUH EMDAP INAM MO" onMouseEnter={shiftInfoPack[1]} />
           </div>
         </Link>
         <Link to='/penetrating_stone'>
           <div className="img_index">
-            <img className="works_exhi_index1" src={penet_main} alt="Penetrating Stone" onMouseEnter={shiftInfo} />
-            <img className="works_exhi_index2" src={penet_sec} alt="Penetrating Stone" onMouseEnter={shiftInfo} />
+            <img className="works_exhi_index1" src={penet_main} alt="Penetrating Stone" />
+            <img className="works_exhi_index2" src={penet_sec} alt="Penetrating Stone" onMouseEnter={shiftInfoPack[1]} />
           </div>
         </Link>
         <Link to='/eternal_classics'>
           <div className="img_index">
-            <img className="works_exhi_index1" src={eternal_main} alt="Eternal classics" onMouseEnter={shiftInfo} />
-            <img className="works_exhi_index2" src={eternal_sec} alt="Eternal classics" onMouseEnter={shiftInfo} />
+            <img className="works_exhi_index1" src={eternal_main} alt="Eternal classics" />
+            <img className="works_exhi_index2" src={eternal_sec} alt="Eternal classics" onMouseEnter={shiftInfoPack[1]} />
           </div>
         </Link>
         <Link to='/group_exhibitions' className='link_index'>
           <div className="img_index">
-            <img className="works_exhi_index1" src={group_death} alt="Group Exhibitions" onMouseEnter={shiftInfo} />
-            <img className="works_exhi_index2" src={group_hang} alt="Group Exhibitions" onMouseEnter={shiftInfo} />
+            <img className="works_exhi_index1" src={group_death} alt="Group Exhibitions" />
+            <img className="works_exhi_index2" src={group_hang} alt="Group Exhibitions" onMouseEnter={shiftInfoPack[1]} />
           </div>
         </Link>
         <Link to='/exhibition'>
           <div className="img_index">
-            <img className="works_exhi_index1" src={exhibi_main} alt="Exhibition Detail" onMouseEnter={shiftInfo} />
+            <img className="works_exhi_index1" src={exhibi_main} alt="Exhibition Detail" onMouseEnter={shiftInfoPack[1]} />
           </div>
         </Link>
       </div>
-      <ExhiInfo info={info} />
+      <ExhiInfo info={shiftInfoPack[0]} />
     </section>
   );
 }
