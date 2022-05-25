d2dad2682c2b4f10104f8ab7e7ff4021d87e3694
youngdong82 <nfs82young@gmail.com>
2022-05-25 17:15:47 +0900

    ⚡️ convert take and load screenshot lighter-2

--- src/components/SwitchTakeSaveBtn.js
+++ src/components/SwitchTakeSaveBtn.js
@@ -2,36 +2,34 @@ import React from 'react';
 import '../style/web/component/switchTakeSaveBtn.css';
 import '../style/mobile/component/switchTakeSaveBtn_m.css';
 
-const SwitchTakeSaveBtn = ({savedReck, showSpread, takeScreenshot, imgsrcToLoad, preventEmpty}) => {
+//redux
+import { connect } from 'react-redux';
 
+const SwitchTakeSaveBtn = ({savedReck_rdx, showSpread, takeAndSave, screenshot, preventEmpty}) => {
   return(
     <>
     <div className="eter_btns">
-      <button className="eter_btn" onClick={showSpread}>
-        {savedReck.length !== 0 ? 
-          <button className="push">
-            {savedReck.length}
-          </button> 
+      <button className="eter_btn fontAwesome fas fa-exchange-alt" onClick={showSpread}>
+        {savedReck_rdx.length !== 0 ? 
+          <div className="push">
+            {savedReck_rdx.length}
+          </div> 
           : <></>}
-        <span>
-          <i className="fontAwesome fas fa-exchange-alt" />
-        </span>
       </button>
-      <button className="eter_btn" onClick={takeScreenshot}>
-        <span>
-          <i className="fontAwesome fas fa-camera" />
-        </span>
-      </button> 
+      <button className="eter_btn fontAwesome fas fa-camera" onClick={takeAndSave} >
+        {/* <a href={imgsrcToLoad} download /> */}
+      </button>
       <button className="eter_btn">
-        <span>
-          <a href={imgsrcToLoad} download onClick={preventEmpty}>
-            <i className="fontAwesome far fa-save" />
+          <a href={screenshot} onClick={preventEmpty} download className="fontAwesome far fa-save">
           </a>
-        </span>
       </button>
     </div>
     </>
   )
 }
 
-export default SwitchTakeSaveBtn
+const mapStateToProps = (state,ownProps) => {
+  return {savedReck_rdx: state}
+}
+
+export default connect(mapStateToProps)(SwitchTakeSaveBtn)
