b70bd0ea28e73eb84488f1c78191ca4e16801182
youngdong82 <nfs82young@gmail.com>
2023-06-18 17:47:29 +0900

    ADD: WAT 세트  import 컴포넌트 생성

---
+++ src/data/Wat_SET.js
@@ -0,0 +1,536 @@
+import React from 'react';
+import wat_001_01 from '../asset/wat/wat_1/wat_001_01.jpg';
+import wat_001_02 from '../asset/wat/wat_1/wat_001_02.jpg';
+
+import wat_002_01 from '../asset/wat/wat_2/wat_002_01.jpg';
+import wat_002_02 from '../asset/wat/wat_2/wat_002_02.jpg';
+import wat_002_03 from '../asset/wat/wat_2/wat_002_03.jpg';
+import wat_002_04 from '../asset/wat/wat_2/wat_002_04.jpg';
+import wat_002_05 from '../asset/wat/wat_2/wat_002_05.jpg';
+import wat_002_06 from '../asset/wat/wat_2/wat_002_06.jpg';
+import wat_002_07 from '../asset/wat/wat_2/wat_002_07.jpg';
+import wat_002_08 from '../asset/wat/wat_2/wat_002_08.jpg';
+import wat_002_09 from '../asset/wat/wat_2/wat_002_09.jpg';
+import wat_002_10 from '../asset/wat/wat_2/wat_002_10.jpg';
+
+import wat_003_01 from '../asset/wat/wat_3/wat_003_01.jpg';
+import wat_003_02 from '../asset/wat/wat_3/wat_003_02.jpg';
+import wat_003_03 from '../asset/wat/wat_3/wat_003_03.jpg';
+import wat_003_04 from '../asset/wat/wat_3/wat_003_04.jpg';
+import wat_003_05 from '../asset/wat/wat_3/wat_003_05.jpg';
+import wat_003_06 from '../asset/wat/wat_3/wat_003_06.jpg';
+import wat_003_07 from '../asset/wat/wat_3/wat_003_07.jpg';
+import wat_003_08 from '../asset/wat/wat_3/wat_003_08.jpg';
+
+import wat_004_01 from '../asset/wat/wat_4/wat_004_01.jpg';
+import wat_004_02 from '../asset/wat/wat_4/wat_004_02.jpg';
+import wat_004_03 from '../asset/wat/wat_4/wat_004_03.jpg';
+import wat_004_04 from '../asset/wat/wat_4/wat_004_04.jpg';
+import wat_004_05 from '../asset/wat/wat_4/wat_004_05.jpg';
+import wat_004_06 from '../asset/wat/wat_4/wat_004_06.jpg';
+import wat_004_07 from '../asset/wat/wat_4/wat_004_07.jpg';
+import wat_004_08 from '../asset/wat/wat_4/wat_004_08.jpg';
+import wat_004_09 from '../asset/wat/wat_4/wat_004_09.jpg';
+import wat_004_10 from '../asset/wat/wat_4/wat_004_10.jpg';
+import wat_004_11 from '../asset/wat/wat_4/wat_004_11.jpg';
+import wat_004_12 from '../asset/wat/wat_4/wat_004_12.jpg';
+
+import wat_005_01 from '../asset/wat/wat_5/wat_005_01.jpg';
+import wat_005_02 from '../asset/wat/wat_5/wat_005_02.jpg';
+import wat_005_03 from '../asset/wat/wat_5/wat_005_03.jpg';
+import wat_005_04 from '../asset/wat/wat_5/wat_005_04.jpg';
+import wat_005_05 from '../asset/wat/wat_5/wat_005_05.jpg';
+import wat_005_06 from '../asset/wat/wat_5/wat_005_06.jpg';
+import wat_005_07 from '../asset/wat/wat_5/wat_005_07.jpg';
+import wat_005_08 from '../asset/wat/wat_5/wat_005_08.jpg';
+import wat_005_09 from '../asset/wat/wat_5/wat_005_09.jpg';
+import wat_005_10 from '../asset/wat/wat_5/wat_005_10.jpg';
+import wat_005_11 from '../asset/wat/wat_5/wat_005_11.jpg';
+
+import wat_006_01 from '../asset/wat/wat_6/wat_006_01.jpg';
+import wat_006_02 from '../asset/wat/wat_6/wat_006_02.jpg';
+import wat_006_03 from '../asset/wat/wat_6/wat_006_03.jpg';
+import wat_006_04 from '../asset/wat/wat_6/wat_006_04.jpg';
+import wat_006_05 from '../asset/wat/wat_6/wat_006_05.jpg';
+import wat_006_06 from '../asset/wat/wat_6/wat_006_06.jpg';
+import wat_006_07 from '../asset/wat/wat_6/wat_006_07.jpg';
+import wat_006_08 from '../asset/wat/wat_6/wat_006_08.jpg';
+
+import wat_007_01 from '../asset/wat/wat_7/wat_007_01.jpg';
+import wat_007_02 from '../asset/wat/wat_7/wat_007_02.jpg';
+import wat_007_03 from '../asset/wat/wat_7/wat_007_03.jpg';
+import wat_007_04 from '../asset/wat/wat_7/wat_007_04.jpg';
+import wat_007_05 from '../asset/wat/wat_7/wat_007_05.jpg';
+import wat_007_06 from '../asset/wat/wat_7/wat_007_06.jpg';
+import wat_007_07 from '../asset/wat/wat_7/wat_007_07.jpg';
+
+import wat_008_01 from '../asset/wat/wat_8/wat_008_01.jpg';
+import wat_008_02 from '../asset/wat/wat_8/wat_008_02.jpg';
+import wat_008_03 from '../asset/wat/wat_8/wat_008_03.jpg';
+import wat_008_04 from '../asset/wat/wat_8/wat_008_04.jpg';
+import wat_008_05 from '../asset/wat/wat_8/wat_008_05.jpg';
+import wat_008_06 from '../asset/wat/wat_8/wat_008_06.jpg';
+import wat_008_07 from '../asset/wat/wat_8/wat_008_07.jpg';
+import wat_008_08 from '../asset/wat/wat_8/wat_008_08.jpg';
+import wat_008_09 from '../asset/wat/wat_8/wat_008_09.jpg';
+import wat_008_10 from '../asset/wat/wat_8/wat_008_10.jpg';
+import wat_008_11 from '../asset/wat/wat_8/wat_008_11.jpg';
+
+
+const Wat_SET = () => {
+    const WAT_SET = [
+        {
+            title:"화보-다시 들어가며-1",
+            caption: "한지에 목판, 유성잉크. 31.2×21.2cm. 2023",
+            src: wat_001_01,
+            ref: wat00101,
+        },
+        {
+            title:"화보-다시 들어가며-2",
+            caption: "한지에 목판, 유성잉크. 31.2×21.2cm. 2023",
+            src: wat_001_02,
+            ref: wat00102,
+        },
+        {
+            title:"화18-1",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_002_01,
+            ref: wat00201,
+        },
+        {
+            title:"화보18-2",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_002_02,
+            ref: wat00202,
+        },
+        {
+            title:"화보18-3",
+            caption: "한지에 목판, 유성잉크. 31.2×21.2cm. 2023",
+            src: wat_002_03,
+            ref: wat00203,
+        },
+        {
+            title:"화보18-4",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_002_04,
+            ref: wat00204,
+        },
+        {
+            title:"화보18-5",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_002_05,
+            ref: wat00205,
+        },
+        {
+            title:"화보18-6",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_002_06,
+            ref: wat00206,
+        },
+        {
+            title:"화보18-7",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_002_07,
+            ref: wat00207,
+        },
+        {
+            title:"화보18-8",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_002_08,
+            ref: wat00208,
+        },
+        {
+            title:"화보18-9",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_002_09,
+            ref: wat00209,
+        },
+        {
+            title:"화보18-10",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_002_10,
+            ref: wat00210,
+        },
+        {
+            title:"화보19-1",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_01,
+            ref: wat00301,
+        },
+        {
+            title:"화보19-2",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_02,
+            ref: wat00302,
+        },
+        {
+            title:"화보19-3",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_03,
+            ref: wat00303,
+        },
+        {
+            title:"화보19-4",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_04,
+            ref: wat00304,
+        },
+        {
+            title:"화보19-5",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_05,
+            ref: wat00305,
+        },
+        {
+            title:"화보19-6",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_06,
+            ref: wat00306,
+        },
+        {
+            title:"화보19-7",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_07,
+            ref: wat00307,
+        },
+        {
+            title:"화보19-8",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_08,
+            ref: wat00308,
+        },
+        {
+            title:"화보19-9",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_09,
+            ref: wat00309,
+        },
+        {
+            title:"화보19-10",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_10,
+            ref: wat00310,
+        },
+        {
+            title:"화보19-11",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_11,
+            ref: wat00311,
+        },
+        {
+            title:"화보19-12",
+            caption: "한지에 목판, 유성잉크. 31.2×21.2cm. 2023",
+            src: wat_003_12,
+            ref: wat00312,
+        },
+        {
+            title:"화보19-13",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_13,
+            ref: wat00313,
+        },
+        {
+            title:"화보19-14",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_003_14,
+            ref: wat00314,
+        },
+        {
+            title:"화보20-1",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_01,
+            ref: wat00401,
+        },
+        {
+            title:"화보20-2",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_02,
+            ref: wat00402,
+        },
+        {
+            title:"화보20-3",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_03,
+            ref: wat00403,
+        },
+        {
+            title:"화보20-4",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_04,
+            ref: wat00404,
+        },
+        {
+            title:"화보20-5",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_05,
+            ref: wat00405,
+        },
+        {
+            title:"화보20-6",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_06,
+            ref: wat00406,
+        },
+        {
+            title:"화보20-7",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_07,
+            ref: wat00407,
+        },
+        {
+            title:"화보20-8",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_08,
+            ref: wat00408,
+        },
+        {
+            title:"화보20-9",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_09,
+            ref: wat00409,
+        },
+        {
+            title:"화보20-10",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_10,
+            ref: wat00410,
+        },
+        {
+            title:"화보20-11",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_11,
+            ref: wat00411,
+        },
+        {
+            title:"화보20-12",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_004_12,
+            ref: wat00412,
+        },
+        {
+            title:"화보21-1",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_005_01,
+            ref: wat00501,
+        },
+        {
+            title:"화보21-2",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_005_02,
+            ref: wat00502,
+        },
+        {
+            title:"화보21-3",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_005_03,
+            ref: wat00503,
+        },
+        {
+            title:"화보21-4",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_005_04,
+            ref: wat00504,
+        },
+        {
+            title:"화보21-5",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_005_05,
+            ref: wat00505,
+        },
+        {
+            title:"화보21-6",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_005_06,
+            ref: wat00506,
+        },
+        {
+            title:"화보21-7",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_005_07,
+            ref: wat00507,
+        },
+        {
+            title:"화보21-8",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_005_08,
+            ref: wat00508,
+        },
+        {
+            title:"화보21-9",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_005_09,
+            ref: wat00509,
+        },
+        {
+            title:"화보21-10",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_005_10,
+            ref: wat00510,
+        },
+        {
+            title:"화보21-11",
+            caption: "한지에 목판, 유성잉크. 31.2×21.2cm. 2023",
+            src: wat_005_11,
+            ref: wat00511,
+        },
+        {
+            title:"화보22-1",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_006_01,
+            ref: wat00601,
+        },
+        {
+            title:"화보22-2",
+            caption: "한지에 목판, 유성잉크. 31.2×21.2cm. 2023",
+            src: wat_006_02,
+            ref: wat00602,
+        },
+        {
+            title:"화보22-3",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_006_03,
+            ref: wat00603,
+        },
+        {
+            title:"화보22-4",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_006_04,
+            ref: wat00604,
+        },
+        {
+            title:"화보22-5",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_006_05,
+            ref: wat00605,
+        },
+        {
+            title:"화보22-6",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_006_06,
+            ref: wat00606,
+        },
+        {
+            title:"화보22-7",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_006_07,
+            ref: wat00607,
+        },
+        {
+            title:"화보22-8",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_006_08,
+            ref: wat00608,
+        },
+        {
+            title:"화보23-1",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_007_01,
+            ref: wat00701,
+        },
+        {
+            title:"화보23-2",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_007_02,
+            ref: wat00702,
+        },
+        {
+            title:"화보23-3",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_007_03,
+            ref: wat00703,
+        },
+        {
+            title:"화보23-4",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_007_04,
+            ref: wat00704,
+        },
+        {
+            title:"화보23-5",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_007_05,
+            ref: wat00705,
+        },
+        {
+            title:"화보23-6",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_007_06,
+            ref: wat00706,
+        },
+        {
+            title:"화보23-7",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_007_07,
+            ref: wat00707,
+        },
+        {
+            title:"화보24-1",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_01,
+            ref: wat00801,
+        },
+        {
+            title:"화보24-2",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_02,
+            ref: wat00802,
+        },
+        {
+            title:"화보24-3",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_03,
+            ref: wat00803,
+        },
+        {
+            title:"화보24-4",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_04,
+            ref: wat00804,
+        },
+        {
+            title:"화보24-5",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_05,
+            ref: wat00805,
+        },
+        {
+            title:"화보24-6",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_06,
+            ref: wat00806,
+        },
+        {
+            title:"화보24-7",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_07,
+            ref: wat00807,
+        },
+        {
+            title:"화보24-8",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_08,
+            ref: wat00808,
+        },
+        {
+            title:"화보24-9",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_09,
+            ref: wat00809,
+        },
+        {
+            title:"화보24-10",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_10,
+            ref: wat00810,
+        },
+        {
+            title:"화보24-11",
+            caption: "한지에 목판, 유성잉크, 수채화물감. 31.2×21.2cm. 2023",
+            src: wat_008_11,
+            ref: wat00811,
+        },
+    ];
+    return WAT_SET;
+}
+
+export default Wat_SET;
