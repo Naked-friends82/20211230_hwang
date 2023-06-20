5249e29bc2dedde0348548605d4bb32c722413fb
youngdong82 <nfs82young@gmail.com>
2023-06-21 00:49:51 +0900

    EDIT: Exhibi에 마우스 휠 횡이동 기능 추가

--- src/routes/Exhibi.js
+++ src/routes/Exhibi.js
@@ -7,6 +7,7 @@ import '../styles/web/exhibi/exhibi.css';
 import '../styles/mobile/exhibi/exhibi_m.css';
 
 // 함수_리펙토링
+import { useHorizontalScroll } from "../function/scroll";
 import ToggleDetail from '../function/ToggleDetail.js';
 
 const Exhibi = () =>  {
@@ -15,13 +16,15 @@ const Exhibi = () =>  {
   // shiftThumbPack[1]은 detail 바꾸는 함수
   const toggleDetailPack = ToggleDetail();
 
+  const scrollRef = useHorizontalScroll();
+
   return (
     <section className="section">
       <div className="section_title">
         <h1>Exhibitions</h1>
         <button onClick={toggleDetailPack[1]}><i className="fa-solid fa-info" /></button>
       </div>
-      <div className="exhibi_reck">
+      <div className="exhibi_reck" ref={scrollRef}>
         <EachExhibi exhiIndex={0} />
         <EachExhibi exhiIndex={1} />
         <EachExhibi exhiIndex={2} />
