58201fe7d78c691b2a46bb5ec6692969ff79d54d
youngdong82 <nfs82young@gmail.com>
2023-06-22 01:25:42 +0900

    EDIT: 캡션 변경 + 스크롤 분리
    무지성 이동에서 캡션 변경+이동, 캡션 변경 으로 분리

--- src/function/ShiftThumb.js
+++ src/function/ShiftThumb.js
@@ -5,21 +5,34 @@ import { scrollIntoView } from "seamless-scroll-polyfill";
 const ShiftThumb = (initial_value, ref_deck) => {
   const [thumb, setThumb] = useState(initial_value);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   for(let i = 0; i < ref_deck.length; i++ ){
+  //     if (thumb === ref_deck[i].current.alt){
+  //       // safari, chrome 둘 다 작동한다.
+  //       scrollIntoView(ref_deck[i].current,{behavior: "smooth", block: "center", inline: "center"});
+  //       // ref_deck[i].current.scrollIntoView({behavior: "smooth", inline: "center"});
+  //     }
+  //   }
+  // }, [thumb])
+
+  const changeThumb = (e) => {
+    const {target:{alt}} = e;
+    setThumb(alt);
+  };
+
+  const scrollToThumb = (e) => {
+    const {target:{alt}} = e;
+    setThumb(alt);
     for(let i = 0; i < ref_deck.length; i++ ){
-      if (thumb === ref_deck[i].current.alt){
+      if (alt === ref_deck[i].current.alt){
         // safari, chrome 둘 다 작동한다.
         scrollIntoView(ref_deck[i].current,{behavior: "smooth", block: "center", inline: "center"});
         // ref_deck[i].current.scrollIntoView({behavior: "smooth", inline: "center"});
       }
     }
-  }, [thumb])
+  }
 
-  const changeThumb = (e) => {
-    const {target:{alt}} = e;
-    setThumb(alt)
-  };
-  const shiftThumbPack = [thumb, changeThumb]
+  const shiftThumbPack = [thumb, changeThumb, scrollToThumb]
 
   return shiftThumbPack
 }
